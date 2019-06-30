c0b10703a57dce636dcc41cfcdfd9a56c85365ac
Albert Nowak <48806977+al1922@users.noreply.github.com>
2019-06-30 16:16:49 +0200

    Update Gravity-Search-Algorithm-final.py

--- Gravity-Search-Algorithm-final.py
+++ Gravity-Search-Algorithm-final.py
@@ -1,8 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from numpy.random import rand, randn, randint
-from numpy.linalg import norm
-
 import numpy as np 
 import matplotlib.pyplot as plt
 from numpy.random import rand, randn, randint
